b3803cbaba3c7988e031e1100f651fd11a762edb
adrians90 <adriansandu9090@gmail.com>
2024-05-11 12:23:34 +0100

    make Textarea a controlled element

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function Textarea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  return (
+    <textarea
+      value={text}
+      onChange={(e) => {
+        const newText = e.target.value;
+        setText(newText);
+      }}
+      className="textarea"
+      placeholder="Enter your text"
+      spellCheck="false"
+    />
+  );
 }
 
 export default Textarea;
