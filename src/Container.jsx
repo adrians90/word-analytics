c5485d33efd8ac9887358a4ed7abf315ed6e9b29
adrians90 <adriansandu9090@gmail.com>
2024-05-11 13:29:30 +0100

    move instagram&facebook max characters into a constants.js file

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./constants";
 
 function Container() {
   const [text, setText] = useState("");
@@ -8,8 +9,8 @@ function Container() {
   const stats = {
     numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
     numberOfCharacters: text.length,
-    instagramCharactersLeft: 280 - text.length,
-    facebookCharactersLeft: 2200 - text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
   };
   return (
     <main className="container">
