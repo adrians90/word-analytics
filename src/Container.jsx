05e9e2530672b120407e2a754d9f78ba1738891d
adrians90 <adriansandu9090@gmail.com>
2024-05-11 13:20:50 +0100

    extract all stats props in an object to clean the code

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,11 +4,21 @@ import Textarea from "./Textarea";
 
 function Container() {
   const [text, setText] = useState("");
+  const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
   const numberOfCharacters = text.length;
+  const instagramCharactersLeft = 280 - text.length;
+  const facebookCharactersLeft = 2200 - text.length;
+
+  const stats = {
+    numberOfWords: numberOfWords,
+    numberOfCharacters: numberOfCharacters,
+    instagramCharactersLeft: instagramCharactersLeft,
+    facebookCharactersLeft: facebookCharactersLeft,
+  };
   return (
     <main className="container">
       <Textarea text={text} setText={setText} />
-      <Stats numberOfCharacters={numberOfCharacters} />
+      <Stats stats={stats} />
     </main>
   );
 }
